2aeac6a0d3f571aece92adb54212cbce8376cf08
RipuDaman Singh <ripu110daman@gmail.com>
2021-12-19 23:38:27 +0530

    Add files via upload

---
+++ foodorder/admin/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Food order website</title>
+	<link rel="stylesheet" type="text/css" href="../css/admin.css">
+</head>
+<body>
+<!---- menu section start--->
+	<div class="menu text-center">
+		<div class="wrapper">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Admin</a></li>
+				<li><a href="#">Category</a></li>
+				<li><a href="#">Food</a></li>
+				<li><a href="#">Order</a></li>
+			</ul>
+		</div>
+</div>
+<!---- menu section ends--->
+
+<!---- main content section starts--->
+	<div class="main-content">
+		<div class="wrapper">
+			<strong>Menu</strong>
+		</div>
+	</div>
+<!---- main content section ends--->
+
+<!---- Footer section start--->
+<div class="footer">
+	<div class="wrapper">
+			<p class="text-center">2021 @LL Right Reserved to FoodOrder. Developed By : <a href="#">Ripudamansingh</a></p>
+		</div>
+	</div>
+<!---- Footer section ends--->
+</body>
+</html>
